a68d2004aa73f358f3b44687dfe09cfd7b4e427c
Goh Jin <jeryljin133799@gmail.com>
2025-11-26 13:12:37 +0800

    updated_lesson3

--- lesson_three.py
+++ lesson_three.py
@@ -11,17 +11,17 @@ win.title("Personal details")
 win.iconphoto(False,p1)
 win.geometry("300x200")
 
-f1 = tk.Frame(width=50, height=40, bg='red')
-f2 = tk.Frame(width=80, height=20, bg='green')
-f3 = tk.Frame(width=20, height=20, bg='blue')
+f1 = tk.Frame(master=win, width=100, height=40, bg='red', padx=5, pady=5)
+f2 = tk.Frame(master=f1, width=30, height=30, bg='green')
+f3 = tk.Frame(master=win, width=200, height=20, bg='blue')
 
 # "fill = tk.X" - fill the whole space with horizontal
 # "fill = tk.Y" - fill the whole space with vertically
 
 # expand = True to expand vertically, space in between them
-f1.pack(fill = tk.Y, )
-f2.pack(fill = tk.Y, )
-f3.pack(fill = tk.Y, )
+f1.pack(fill = tk.BOTH, expand=True, side=tk.LEFT)
+f2.place(x =  30, y = 30)
+f3.pack(fill = tk.BOTH, expand=True, side=tk.LEFT)
 
 #looping of window
 win.mainloop()
