890a9f14043f34c456f0a1a303ee14986ec85888
Goh Jin <jeryljin133799@gmail.com>
2025-11-27 12:37:01 +0800

    Wrote down the guides message

--- lesson_three.py
+++ lesson_three.py
@@ -22,23 +22,48 @@ import tkinter as tk
 # - helps on adjust the size of compartment to fir the other widget include the other frame.
 # - same like a picture frame, we can adjust and construct the structure we want.
 
-# SIMPLE FRAME() WIDGET
+# - Frame() is like a box or compartment inside your main window.
+# - You will not "see" a frame, unless you give a background color)
+# - Use to group widgets together, control layout easily, build complex UIs
+
+# Conclusion: Frame is a container widget that helps you organise the layout
+#             and structure of your GUI.
+
+# Creating the frame (Sample) (will not visible, until geometry manager)
 #f1 = tk.Frame(width=50, height=40, bg='red')
 #f2 = tk.Frame(width=80, height=20, bg='green')
 #f3 = tk.Frame(width=20, height=20, bg='blue')
 
-# CHOOSE EITHER ONE TO FILL EITHER X(HORIZONTALLY) OR Y(VERTICALLY)
+# WARNING: A frame will not appear, until you give it a geometry manager.
+# Geometry Manager: "pack()", "grid()", & "place()"
+
+# Difference Between tk.X, tk.Y, tk.BOTH
 #---------------------------------------------
-# IT WILL FILL THE FRAME TO THE X (HORIZONTAL FILLED ONLY)
+# The frame stretches horizontally to fill all the width of the space it is given
 #f1.pack(fill = tk.X)
 #f2.pack(fill = tk.X)
 #f3.pack(fill = tk.X)
 
-# IT WILL FILL THE FRAME TO THE Y (VERTICALLY FILLED ONLY)
+# The frame stretches vertically to fill all the height of the space it is given
 #f1.pack(fill = tk.Y)
 #f2.pack(fill = tk.Y)
 #f3.pack(fill = tk.Y)
+
+# The frame stretches both horizontally and vertically
+#f1.pack(fill = tk.BOTH)
+#f2.pack(fill = tk.BOTH)
+#f3.pack(fill = tk.BOTH)
 #---------------------------------------------
+
+# "expand = True / False" = 
+# “When the window has extra space, which widgets should get that extra space?”
+
+#expand = False
+#The widget keeps only the space it needs. Extra space in the window will 
+#remain empty (blank) around the widget, usually at the opposite side.
+
+#expand = True
+#Tells Tkinter: “When there is extra space, please give some of that space to this widget.”
 
 # IT WILL FILL, EVEN IF YOU INCREASE THE SIZE OF WINDOW, IT WILL STICK AND FILL
 #f1.pack(fill = tk.Y, expand = True)
@@ -62,15 +87,22 @@ win.title("Personal details")
 win.iconphoto(False,p1)
 win.geometry("300x200")
 
+# master - is to defines which container this widget should live insides.
+
+# Structure, based on our current settings
+# win
+# -  f1
+#   -  f2
+# -  f3
+
 f1 = tk.Frame(master=win, width=100, height=40, bg='red', padx=5, pady=5)
+# padx & pady is to keep settings of internal padding inside
+# which means, anything inside of "f1", will be 5 pixel away from x and y
 f2 = tk.Frame(master=f1, width=30, height=30, bg='green')
 f3 = tk.Frame(master=win, width=200, height=20, bg='blue')
 
-# "fill = tk.X" - fill the whole space with horizontal
-# "fill = tk.Y" - fill the whole space with vertically
-
-# expand = True to expand vertically, space in between them
 f1.pack(fill = tk.BOTH, expand=True, side=tk.LEFT)
+#  will have 30 pixel away from left edge, and top edge of "f1"
 f2.place(x =  30, y = 30)
 f3.pack(fill = tk.BOTH, expand=True, side=tk.LEFT)
 
