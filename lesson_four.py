22b9ceb920a6fced6667e6b44d9df6de5652b8e5
Goh Jin <jeryljin133799@gmail.com>
2025-12-04 17:12:28 +0800

    sss

--- lesson_four.py
+++ lesson_four.py
@@ -14,13 +14,18 @@ win.geometry("300x200")
 #main
 for i in range(5):
     for j in range(5):
-        tk.Button(text='text').grid(row=i, column=j)
+        tk.Button(
+            master = win,
+            text= f'Btn {1+(i*5)+j}',
+            borderwidth=5).grid(row=i, column=j, padx=1, pady=1)
 
-l1 = tk.Label(text="GUI", bg='yellow')
-l2 = tk.Label(text="GUI", bg = 'red')
 
-l1.grid(row=0, column=0)
-l2.grid(row=1, column=1)
+#l1 = tk.Label(text="GUI", bg='yellow')
+#l2 = tk.Label(text="GUI", bg = 'red')
+
+#coordinate placement
+#l1.grid(row=0, column=0)
+#l2.grid(row=1, column=1)
 
 #looping of window
 win.mainloop()
