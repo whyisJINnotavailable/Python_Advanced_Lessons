4db5ca7f8eb140108fb2d3d33408e08055d0fa96
Goh Jin <jeryljin133799@gmail.com>
2025-12-04 18:03:52 +0800

    lessonfour

--- lesson_four.py
+++ lesson_four.py
@@ -12,12 +12,23 @@ win.title("Window Title")
 win.geometry("300x200")
 
 #main
-for i in range(5):
-    for j in range(5):
+#for i in range(5):
+#    tk.Button(text="BTN").grid(row=i, column=0)
+
+#for j in range(5):
+#    tk.Button(text="BTN").grid(row=0, column=j)
+
+#stk = ['e','w','n','s']
+stk = ['ne','ew','ns','nsew']
+for i in range(2):
+    win.rowconfigure(index=i, weight=1, minsize=200/5)
+    for j in range(2):
+        win.columnconfigure(index=i, weight=1, minsize=300/5)
         tk.Button(
             master = win,
+            #this will make each button have numbers name on it
             text= f'Btn {1+(i*5)+j}',
-            borderwidth=5).grid(row=i, column=j, padx=1, pady=1)
+            borderwidth=5).grid(row=i, column=j, padx=1, pady=1, sticky= stk[(i*2)+j])
 
 
 #l1 = tk.Label(text="GUI", bg='yellow')
