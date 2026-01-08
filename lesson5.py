a61bf450d7bc2061b9e272b24b087ab5caf3efa9
Goh Jin <jeryljin133799@gmail.com>
2026-01-08 10:07:09 +0800

    update lesson 5

--- lesson5.py
+++ lesson5.py
@@ -2,11 +2,16 @@
 
 # =========================
 # 1. Modules imports
-import tkinter as tk
+# - Import the libraries we need (tkinter, random, time)
 # =========================
+import tkinter as tk
 
 # =========================
 # 2. Game window setup
+# - Create main window
+# - Set settings of window (title, size, icon)
+# - 
+# =========================
 win = tk.Tk()
 win.title("Tkinter Game - Canvas Intro")
 win.geometry("600x200")
@@ -15,59 +20,43 @@ try:
     win.iconphoto(False,p1)
 except:
     pass
-# =========================
 
 # =========================
 # 3. Game canvas and objects
+# - Create the canvas & pack them into the window
+# - Create player, enemies, items, buttons, text,...
+# - Store object IDs returned by canvas.create
+# =========================
 W = 600
 H = 200
 canvas = tk.Canvas(win, width=W, height=H, bg="black")
 canvas.pack()
 
-player = canvas.create_rectangle(200,180,320,220, fill="white")
+player = canvas.create_rectangle(200,180,320,160, fill="white")
 
-# =========================
 
 # =========================
 # 4. Game variables (score, lives, state)
-speed = 6
+# - Create variables that control the game (speed, game_state, running,...)
+# - Create key dictionary
+# =========================
+speed = 10
 keys = {"Left": False, "Right": False, "Up": False, "Down": False}
 running = True
-# =========================
 
 # =========================
-# 5. Helper function (collisions and score)
-def clamp_item(item_id):
-    x1, y1, x2, y2 = canvas.coords(item_id)
-    dx = 0
-    dy = 0
-    if x1 < 0: dx = -x1
-    if x2 > W: dx = W - x2
-    if y1 < 0: dy = -y1
-    if y2 > H: dy = H - y2
-    if dx or dy:
-        canvas.move(item_id, dx, dy)
+# 5. Helper function (player movement)
+# - Functions that help the game run smoothly (move player, check collisions,...)
 # =========================
 
 # =========================
 # 6. Main GameLoop (update and draw)
+# ========================
 def game_loop():
     if not running:
         return
-
-    dx = 0
-    dy = 0
-
-    if keys["Left"]:  dx -= speed
-    if keys["Right"]: dx += speed
-    if keys["Up"]:    dy -= speed
-    if keys["Down"]:  dy += speed
-
-    canvas.move(player, dx, dy)
-    clamp_item(player)
-
+    move_player()
     win.after(16, game_loop)
-# =========================
 
 # =========================
 # 7. Player input (buttons and keyboard)
@@ -78,6 +67,22 @@ def on_key_press(event):
 def on_key_release(event):
     if event.keysym in keys:
         keys[event.keysym] = False
+
+def move_player():
+    # dx = 0
+    # dy = 0
+    dx = dy = 0
+
+    if keys["Left"]:
+        dx -= speed
+    if keys["Right"]:
+        dx += speed
+    if keys["Up"]:
+        dy -= speed
+    if keys["Down"]:
+        dy += speed
+    
+    canvas.move(player, dx, dy)
 
 win.bind("<KeyPress>", on_key_press)
 win.bind("<KeyRelease>", on_key_release)
